08def2045919093887735e85d75c0f7b55d84551
Mr Das <81897206+Mr-argha-das@users.noreply.github.com>
2025-07-23 23:45:39 +0530

    ghvhohou

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ggc',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
